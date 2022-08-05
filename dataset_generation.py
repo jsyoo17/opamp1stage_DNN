d09986c353a0576f6bfa9962b0ba12bad03de544
JunsangYoo-Dylan <86601561+JunsangYoo-Dylan@users.noreply.github.com>
2022-08-06 02:20:37 +0900

    train model 1 added. With normalization

--- dataset_generation.py
+++ dataset_generation.py
@@ -9,6 +9,8 @@ Used for predicting OPAMP MOSFET sizes depending on performances
 import numpy as np
 import matplotlib.pyplot as plt
 import os
+import warnings
+warnings.filterwarnings('ignore')
 
 # pyspice import
 import PySpice.Logging.Logging as Logging
@@ -17,8 +19,11 @@ from PySpice.Spice.Netlist import Circuit
 from PySpice.Unit import *
 from PySpice.Spice.Library import SpiceLibrary
 
-# os.chdir(os.path.dirname(__file__))
+# os.chdir(os.path.dirname(__file__))     # change directory to where current file is
 spice_library = SpiceLibrary('./')      # use TSMC 180nm library file (measured)
+
+# import simulation function
+from simulation import simulate
 
 ## OPAMP design parameters
 '''
@@ -47,13 +52,14 @@ This uses TSMC 180nm device
 For 180nm devices, the working voltage should be 1.8V
 minimum Length: 180nm
 minimum width: 220nm
+constraints for differential pair: W1=W2, W3=W4
 '''
 
 VDD=1.8; VSS=0
 ibias=20e-6
 Lmin=180e-9; Wmin=220e-9
-W=np.array([10e-6,10e-6,0.42e-6,0.42e-6,10e-6,5e-6])    # W1~W6
-L=np.array([Lmin,Lmin,Lmin,Lmin,1e-6,1e-6])     # L1~L6
+W_samp=np.array([10e-6,10e-6,0.42e-6,0.42e-6,10e-6,5e-6])   # sample W1~W6
+L_samp=np.array([Lmin,Lmin,Lmin,Lmin,1e-6,1e-6])            # sample L1~L6
 MOScnt=6  # total number of MOSFET
 
 ## Circuit definition
@@ -62,181 +68,58 @@ opamp.include(spice_library['nch'])   # this includes library file that has nch 
 
 # OPAMP
 opamp.I(1,'DD',3,ibias)     # current source
-opamp.M(1,1,'INP',2,2,model='nch',l=L[0],w=W[0])          # differential pair
-opamp.M(2,'OUT','INN',2,2,model='nch',l=L[1],w=W[1])      # differential pair
-opamp.M(3,1,1,'DD','DD',model='pch',l=L[2],w=W[2])        # active load
-opamp.M(4,'OUT',1,'DD','DD',model='pch',l=L[3],w=W[3])    # active load
-opamp.M(5,2,3,'SS','SS',model='nch',l=L[4],w=W[4])        # current source
-opamp.M(6,3,3,'SS','SS',model='nch',l=L[5],w=W[5])        # current mirror
+M1=opamp.M(1,1,'INP',2,2,model='nch',l=L_samp[0],w=W_samp[0])          # differential pair
+M2=opamp.M(2,'OUT','INN',2,2,model='nch',l=L_samp[1],w=W_samp[1])      # differential pair
+M3=opamp.M(3,1,1,'DD','DD',model='pch',l=L_samp[2],w=W_samp[2])        # active load
+M4=opamp.M(4,'OUT',1,'DD','DD',model='pch',l=L_samp[3],w=W_samp[3])    # active load
+M5=opamp.M(5,2,3,'SS','SS',model='nch',l=L_samp[4],w=W_samp[4])        # current source
+M6=opamp.M(6,3,3,'SS','SS',model='nch',l=L_samp[5],w=W_samp[5])        # current mirror
 opamp.C(1,'OUT',0,100e-15)  # loading cap
 
 print(opamp)
 
-## Simulation Function
-def simulate(opamp, W, L):
-    '''
-    opamp simulation function
-    arguments:
-        opamp: opamp circuit to simulate. will be cloned and than used.
-        W: array of widths of MOSFETs in opamp
-        L: array of lengths of MOSFETs in opamp
-    returns:
-        dictionary of performances
-    '''
-    perf={}
-    circuit=opamp.clone()
+# test simulate function
+perf=simulate(opamp)
+print(perf)
 
-    # ************************
-    # Operating Point Analysis
-    # ************************
-    tb_op=opamp.clone(title='testbench operating point')
+## CSV Generation Parameters
+W1range=np.round(np.arange(1e-6,11e-6,1e-6),10)
+W3range=np.round(np.arange(0.3e-6,5e-6,0.3e-6),10)
+W5range=np.round(np.arange(2e-6,11e-6,2e-6),10)
+W6range=W5range
 
-    # Voltage Sources
-    tb_op.V(1,'DD',0,VDD)       # VDD
-    tb_op.V(2,'SS',0,0)         # VSS
+## Dataset Generation
+itercnt=0
+with open('dataset.csv', 'w') as f:
+    # title
+    f.write('w1,w2,w3,w4,w5,w6,')
+    for key in perf.keys():
+        f.write(key+',')
+    f.write('\n')
+    for w1 in W1range:
+        for w3 in W3range:
+            for w5 in W5range:
+                for w6 in W6range:
+                    itercnt+=1
+                    w2=w1; w4=w3
+                    M1.width=w1
+                    M2.width=w2
+                    M3.width=w3
+                    M4.width=w4
+                    M5.width=w5
+                    M6.width=w6
 
-    # Input Voltage
-    tb_op.R(1,'DD','INP',50@u_kOhm)
-    tb_op.R(2,'INN','SS',50@u_kOhm)
-    tb_op.R(3,'INP','INN',0@u_Ohm)     # added to regard nodes INP and INN as same node
+                    if (1+itercnt)%100==0:
+                        print(f'sim {itercnt+1}')
+                    try:
+                        perf=simulate(opamp)
+                        f.write(f'{w1},{w2},{w3},{w4},{w5},{w6},')
+                        for val in perf.values():
+                            if type(val)==list:
+                                f.write(str(np.sum(val))+',')
+                            else:
+                                f.write(f'{val:.5e},')
+                        f.write('\n')
+                    except:
+                        print(f'#{itercnt} sim failed')
 
-    # Params to Save
-    simulator= tb_op.simulator(temperature = 25, nominal_temperature =25)
-    internal_params='all'
-    for i in range(MOScnt):
-        internal_params+=(f' @m{int(i+1)}[vds] @m{int(i+1)}[vdsat] @m{int(i+1)}[vth] @m{int(i+1)}[vgs] @m{int(i+1)}[id] @m{int(i+1)}[gm] @m{int(i+1)}[gds]')
-    simulator.save_internal_parameters(internal_params)
-
-    # Operating Point simulation
-    tb_op_sim=simulator.operating_point()
-    perf['op_vout']=float(tb_op_sim.out)
-
-    # saturation check
-    satlist=[]
-    for i in range(MOScnt):
-        cond1=float(tb_op_sim[f'@m{int(i+1)}[vds]'])>float(tb_op_sim[f'@m{int(i+1)}[vdsat]'])
-        cond2=float(tb_op_sim[f'@m{int(i+1)}[vgs]'])>float(tb_op_sim[f'@m{int(i+1)}[vth]'])
-        if cond1 and cond2:
-            satlist.append(1)
-        else:
-            satlist.append(0)
-    perf['op_satlist']=satlist
-
-    # gain calculation
-    ro2=1/float(tb_op_sim['@m2[gds]'])
-    ro4=1/float(tb_op_sim['@m4[gds]'])
-    Ro=ro2*ro4/(ro2+ro4)
-    op_gain=float(tb_op_sim['@m2[gm]'])*Ro
-    op_gaindB=20*np.log10(op_gain)
-    perf['op_gain']=op_gain
-    perf['op_gain_db']=op_gaindB
-
-    # ICMR
-    op_minVINN=VSS+float(tb_op_sim['@m5[vdsat]'])+float(tb_op_sim['@m2[vgs]'])
-    op_maxVINN=VDD-float(tb_op_sim['@m4[vdsat]'])-float(tb_op_sim['@m2[vdsat]'])+float(tb_op_sim['@m2[vgs]'])
-    perf['op_inmin']=op_minVINN
-    perf['op_inmax']=op_maxVINN
-    # todo: perf['op_icmr']
-
-    # Output Range
-    op_minOUT=VSS+float(tb_op_sim['@m5[vdsat]'])+float(tb_op_sim['@m2[vds]'])
-    op_maxOUT=VDD-float(tb_op_sim['@m4[vdsat]'])
-    perf['op_outmin']=op_minOUT
-    perf['op_outmax']=op_maxOUT
-    # todo: perf['op_outrange']
-
-    # ***********
-    # DC Analysis
-    # ***********
-    tb_dc=opamp.clone(title='testbench dc')
-
-    # Voltage Sources
-    tb_dc.V(1,'DD',0,VDD)       # VDD
-    tb_dc.V(2,'SS',0,0)         # VSS
-    tb_dc.VCVS(1,'INP','COMM','DIFF',0,0.5)
-    tb_dc.VCVS(2,'INN','COMM','DIFF',0,-0.5)
-    tb_dc.V(3,'DIFF',0,0)
-    tb_dc.V(4,'COMM',0,0.9)
-
-    # Differential Input
-    simulator=tb_dc.simulator(temperature=25, nominal_temperature=25)
-    tb_dc_sim=simulator.dc(V3=slice(-VDD,VDD,0.01))
-    outdiff_wave=np.diff(tb_dc_sim['out'])/0.01
-    outdiff=[]
-    for i in outdiff_wave:
-        outdiff.append(i.value)
-    outdiff=np.array(outdiff)
-    outmaxidx=np.where(outdiff>1)[0][-1];   dc_outmax=tb_dc_sim['out'][outmaxidx].value
-    outminidx=np.where(outdiff>1)[0][0];    dc_outmin=tb_dc_sim['out'][outminidx].value
-    perf['dc_outmin']=dc_outmin
-    perf['dc_outmax']=dc_outmax
-    # todo: perf['dc_outrange']
-
-    # Common Input
-    tb_dc_sim=simulator.dc(V4=slice(0,VDD,0.01))
-    incomm_wave=np.diff(tb_dc_sim['INP']-tb_dc_sim['2'])/0.01   # Vgs of M1
-    incomm=[]
-    for i in incomm_wave:
-        incomm.append(i.value)
-    incomm=np.array(incomm)
-    inmaxidx=np.where(incomm<0.5)[0][-1];   dc_inmax=tb_dc_sim['INP'][inmaxidx].value
-    inminidx=np.where(incomm<0.5)[0][0];    dc_inmin=tb_dc_sim['INP'][inminidx].value
-    perf['dc_inmin']=dc_inmin
-    perf['dc_outmax']=dc_outmax
-
-    # ******************
-    # Transient Analysis
-    # ******************
-    tb_tran=opamp.clone(title='testbench tran')
-
-    # Voltage Sources
-    tb_tran.V(1,'DD',0,VDD)       # VDD
-    tb_tran.V(2,'SS',0,0)         # VSS
-    tb_tran.VCVS(1,'INP','COMM','DIFF',0,0.5)
-    tb_tran.VCVS(2,'INN','COMM','DIFF',0,-0.5)
-    tran_vsource=tb_tran.SinusoidalVoltageSource(3,'DIFF',0,amplitude=20@u_mV,frequency=100@u_Hz)
-    tb_tran.V(4,'COMM',0,0.9)
-
-    # Small Signal
-    simulator=tb_tran.simulator(temperature=25,nominal_temperature=25)
-    tb_tran_sim=simulator.transient(step_time=tran_vsource.period/200,end_time=tran_vsource.period*2)
-    voutmean=np.mean(tb_tran_sim['out'])
-    voutmax=np.max(tb_tran_sim['out'])
-    tran_gain=float(voutmax-voutmean)/20e-3
-    tran_gaindB=20*np.log10(tran_gain)
-    perf['tran_gain']=tran_gain
-    perf['tran_gain_db']=tran_gaindB
-
-    # ***********
-    # AC Analysis
-    # ***********
-    tb_ac=opamp.clone(title='testbench ac')
-
-    # Voltage Sources
-    tb_ac.V(1,'DD',0,VDD)       # VDD
-    tb_ac.V(2,'SS',0,0)         # VSS
-    tb_ac.VCVS(1,'INP','COMM','DIFF',0,0.5)
-    tb_ac.VCVS(2,'INN','COMM','DIFF',0,-0.5)
-    tb_ac.SinusoidalVoltageSource(3,'DIFF',0,amplitude=1@u_mV,frequency=1@u_kHz)
-    tb_ac.V(4,'COMM',0,0.9)
-
-    # gain
-    simulator=tb_ac.simulator(temperature=25,nominal_temperature=25)
-    tb_ac_sim=simulator.ac(start_frequency=1@u_Hz, stop_frequency=10@u_GHz, number_of_points=21,  variation='dec')
-    ac_vout=tb_ac_sim['out']
-    ac_gain=float(np.abs(ac_vout)[0])
-    ac_gaindB=20*np.log10(ac_gain)
-    perf['ac_gain']=ac_gain
-    perf['ac_gain_db']=ac_gaindB
-
-    # GBP and Phase Margin
-    for idx0db in range(len(tb_ac_sim['out'])):
-        if np.abs(tb_ac_sim['out'])[idx0db]<=1:
-            break
-    ac_GBP=float(tb_ac_sim.frequency[idx0db])
-    ac_PM=np.angle(tb_ac_sim['out'],deg=True)[idx0db]+180
-    perf['ac_GBP']=ac_GBP
-
-    return perf
-
-## CSV generation
---
+++ train_mod2.py
@@ -0,0 +1,177 @@
+'''
+1stage opamp train NN
+Used for predicting OPAMP MOSFET sizes depending on performances
+2022 Junsang Yoo
+
+train NN model 2 takes performances as input and opamp size parameters as output
+loss function compares the following two values:
+ - performance calculation using predicted opamp size parameters
+ - performance that is in dataset csv file (same as the input of the NN)
+'''
+# todo: some consistancy to variable name
+
+## imports
+import numpy as np
+import os
+import matplotlib.pyplot as plt
+
+# pytorch import
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.utils.data import Dataset, DataLoader, random_split
+
+dev=torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+print(dev)
+
+## Dataset Loader
+# os.chdir(os.path.dirname(__file__))     # change directory to where current file is
+
+class LoadData(Dataset):
+    def __init__(self, filename):
+        with open(filename, 'r') as f:
+            # name of each data
+            keys=f.readline()
+            keys=keys[:-2].split(',')
+            self.param_names=keys[:6]
+            self.perf_names=keys[6:]
+
+            # dataset
+            rawdata=f.read()
+            rawdata=rawdata.split()
+            data=[]
+            for line in rawdata:
+                data.append(line[:-1].split(','))
+            data=np.array(data,dtype=np.float32)
+            self.X=data[:,6:]       # performances
+            self.Y=data[:,:6]       # circuit parameters (sizes)
+
+    def __len__(self):
+        return len(self.X)
+
+    def __getitem__(self,idx):
+        x=torch.FloatTensor(self.X[idx], device=dev)
+        y=torch.FloatTensor(self.Y[idx], device=dev)
+        return x,y
+
+    def getrand(self,num):
+        # get 'num' amount of random data
+        l=len(self.X)
+        randidx=np.arange(l)
+        np.random.shuffle(randidx)
+        randx=torch.FloatTensor(self.X[randidx[:num]], device=dev)
+        randy=torch.FloatTensor(self.Y[randidx[:num]], device=dev)
+        return randx,randy
+
+# Split Dataset Into Train, Valid, Test
+# (train : valid : test) = (6 : 2 : 2)
+data=LoadData('dataset.csv')
+ntrain=len(data)//10*6; nvalid=len(data)//10*2; ntest=len(data)-ntrain-nvalid
+traind, validd, testd = random_split(data, [ntrain,nvalid,ntest])
+
+# Load Dataset
+BATCH=256
+traindata=DataLoader(traind, batch_size=BATCH, shuffle=True)
+validdata=DataLoader(validd, batch_size=100)
+testdata=DataLoader(testd, batch_size=100)
+print(f'data splitted. ntrain : nvalid : ntest = 6 : 2 : 2 = {ntrain} : {nvalid} : {ntest}')
+
+## Normalization
+'''
+Normalization process
+*****GBP should be dealt in logarithmic scale?*****
+Normalization layer before input layer and after output layer when model definition
+uses mean and std values found in this section
+'''
+x1000=data.getrand(1000)[0]
+xstd=torch.std(x1000,axis=0)
+xmean=torch.mean(x1000,axis=0)
+y1000=data.getrand(1000)[1]
+ystd=torch.std(y1000,axis=0)
+ymean=torch.mean(y1000,axis=0)
+# normx1000=(x1000-xmean)/xstd
+# normy1000=(y1000-ymean)/ystd
+
+## Neural Net
+class MODEL(nn.Module):
+    def __init__(self):
+        super(MODEL, self).__init__()
+        self.hidden=nn.Sequential(
+            nn.Linear(in_features=17, out_features=256),
+            nn.ReLU(),
+            nn.Linear(in_features=256, out_features=256),
+            nn.ReLU(),
+            nn.Linear(in_features=256, out_features=256),
+            nn.ReLU(),
+            nn.Linear(in_features=256, out_features=64),
+            nn.ReLU(),
+            nn.Linear(in_features=64, out_features=16),
+            nn.ReLU(),
+            )
+        self.outlayer=nn.Linear(in_features=16, out_features=6)
+
+    def forward(self, x):
+        in_norm=(x-xmean)/xstd
+        out = self.hidden(in_norm)
+        out = self.outlayer(out)
+        out_denorm=out*ystd+ymean
+        return out_denorm
+
+model=MODEL()
+model.to(dev)
+torch.cuda.empty_cache()
+
+## Train
+EPOCH=50
+opt = torch.optim.Adam(model.parameters())
+loss_fn = nn.MSELoss().to(dev)
+trainloss=[]
+validloss=[]
+
+# Iterations
+for epoch in range(EPOCH):
+    model.train()
+    meanloss=[]
+
+    # train
+    for batch_idx, (batch_x, batch_y) in enumerate(traindata):
+        opt.zero_grad()         # clear gradient
+        batch_x=batch_x.to(dev); batch_y=batch_y.to(dev)
+        pred=model(batch_x)
+        loss=loss_fn(pred, batch_y)
+        loss.backward()         # Backward Propagation: update NN weight and bias
+        opt.step()              # next step for optimizer
+
+        # to keep data
+        meanloss.append(loss.detach().numpy())
+    trainloss.append(np.mean(meanloss))
+
+    # validation
+    for batch_idx, (batch_x, batch_y) in enumerate(validdata):
+        batch_x=batch_x.to(dev); batch_y=batch_y.to(dev)
+        pred=model(batch_x)
+        loss=loss_fn(pred, batch_y)
+
+        # to keep data
+        meanloss.append(loss.detach().numpy())
+    validloss.append(np.mean(meanloss))
+
+## Test
+for batch_idx, (batch_x, batch_y) in enumerate(testdata):
+    batch_x=batch_x.to(dev); batch_y=batch_y.to(dev)
+    pred=model(batch_x)
+    loss=loss_fn(pred,batch_y)
+    meanloss.append(loss.detach().numpy())
+
+testloss=np.mean(meanloss)
+
+print('\nbatch_x:')
+print(batch_x[:10])
+print('\npred:')
+print(pred[:10])
+print('\nbatch_y:')
+print(batch_y[:10])
+
+## Results
+plt.plot(trainloss)
+plt.show()
